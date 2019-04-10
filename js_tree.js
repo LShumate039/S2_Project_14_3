3fe9a91c84d282ed2bb1e2aa9454065065a2928f
Unknown <LShumate039@west-mec.org>
2019-04-09 18:10:12 -0700

    final commit
    ready to be graded

--- js_tree.js
+++ js_tree.js
@@ -36,32 +36,74 @@
       Returns true if tString represents the text of a white space text
       node and false if it doesn't
 */
-window.onload = makeTree;
+
 
 var nodeCount = 0;
 var elemCount = 0;
 var textCount = 0;
 var wsCount = 0;
+//runs the makeTree() function when the page loads 
 
+window.onload = makeTree;
 
 
 function makeTree() {
       var hello = document.createElement("aside");
-      hello.setAttribute("id", "treebox")
+      hello.setAttribute("id", "treeBox")
       var helloAgain = document.createElement("h1");
       helloAgain.textContent = "Node Tree";
       hello.appendChild(helloAgain);
 
       document.getElementById("main").appendChild(hello);
+      //will be the foundation of the node tree and append it to the aside element fragment 
       var nodeList = document.createElement("ol");
       hello.appendChild(nodeList);
-      var sourceArticle = document.querySelectorAll("#main article")
-
+      var sourceArticle = document.querySelector("#main article")
+      // The treeNode parameter stores the current node from the source article and the nestedList parameter stores the structure of the node tree displayed in the web page 
       makeBranches(sourceArticle, nodeList);
+
+      document.getElementById("totalNodes").textContent = nodeCount;
+      document.getElementById("elemNodes").textContent = elemCount;
+      document.getElementById("textNodes").textContent = textCount;
+      document.getElementById("wsNodes").textContent = wsCount;
+
+
 }
 
 function makeBranches(treeNode, nestedList) {
-      nodeCount += 1;
+      nodeCount++;
+      var liElem = document.createElement("li");
+      liElem.innerHTML += "+--";
+      var spanElem = document.createElement("span");
+      liElem.appendChild(spanElem);
+      nestedList.appendChild(liElem);
+
+      if (treeNode.nodeType === 1) {
+            elemCount++;
+            spanElem.setAttribute("class", "elementNode");
+            spanElem.textContent = "<" +
+                  treeNode.nodeName + ">";
+      } else if (treeNode.nodeType === 3) {
+            textCount++;
+            var textString = treeNode.nodeValue;
+            if (isWhiteSpaceNode(textString)) {
+                  wsCount++;
+                  spanElem.setAttribute("class", "whiteSpaceNode");
+                  spanElem = "#text";
+            } else {
+                  spanElem.setAttribute("class", "textNode");
+                  spanElem.textContent = textString;
+            }
+      }
+      //The makeBranches() function should recursively move through the different levels of nodes in the source article 
+      if (treeNode.childNodes.length > 0) {
+            var newList = document.createElement("ol");
+            newList.innerHTML = "|";
+            nestedList.appendChild(newList);
+            for (var n = treeNode.firstChild; n !== null; n = n.nextSibling) {
+                  makeBranches(n, newList);
+            }
+      }
 
 }
 
